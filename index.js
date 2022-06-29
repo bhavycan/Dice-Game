582af825d9b9ad3c1f550fc980f24cc68375f107
bhavycan <94931828+bhavycan@users.noreply.github.com>
2022-06-29 17:34:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
+var button = document.getElementById("button-roll");
+
+function rollDice(){
+
+
 var number1 =Math.floor( (Math.random() * 6)) + 1;
 
 var number2 =Math.floor( (Math.random() * 6)) + 1;
@@ -12,17 +17,37 @@ var message = "";
 if(number1 > number2){
     message = "Player1 is The Winner!";
     document.querySelectorAll('img')[0].style.transition ="all ease-in-out 0.5s";
+    document.querySelectorAll('img')[0].style.transform ="rotate(135deg)";
     document.querySelectorAll('img')[0].style.width ="70%";
+    document.querySelectorAll('img')[1].style.transition ="all ease-in-out 0.5s";
+    document.querySelectorAll('img')[1].style.transform ="rotate(135deg)";
+    document.querySelectorAll('img')[1].style.width ="50%";
 
 }
 else if(number1 < number2){
     message = "Player2 is The Winner!";
+    document.querySelectorAll('img')[0].style.transition ="all ease-in-out 0.5s";
+    document.querySelectorAll('img')[0].style.transform ="rotate(135deg)";
+    document.querySelectorAll('img')[0].style.width ="50%";
     document.querySelectorAll('img')[1].style.transition ="all ease-in-out 0.5s";
+    document.querySelectorAll('img')[1].style.transform ="rotate(135deg)";
     document.querySelectorAll('img')[1].style.width ="70%";
+    
    
 }
 else{
     message = "Draw!!!";
+    document.querySelectorAll('img')[0].style.transition ="all ease-in-out 0.5s";
+   
+    document.querySelectorAll('img')[0].style.width ="50%";
+    document.querySelectorAll('img')[1].style.transition ="all ease-in-out 0.5s";
+    document.querySelectorAll('img')[1].style.width ="50%";
 }
 
 document.querySelector('.messages p').innerHTML = message;
+
+}
+
+
+button.addEventListener("click", rollDice);
+
